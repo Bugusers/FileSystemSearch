923b63b28b231ed85c3871e51b1efc7d9a8f5b26
Raccoon <vsafirko@gmail.com>
2023-11-11 21:13:48 +0200

    Changes in pom.xml

--- src/main/java/org/lib/Main.java
+++ src/main/java/org/lib/Main.java
@@ -3,7 +3,6 @@ package org.lib;
 import org.lib.filesystemsearch.FileSystemSearch;
 import org.lib.filesystemsearch.FileSystemSearchWithTreadPool;
 
-import java.net.URL;
 import java.util.List;
 
 public class Main {
